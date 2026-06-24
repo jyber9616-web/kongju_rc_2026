52864b33760e85b22ec4395ea659bb3f1645130e
jyber9616-web <jyber9616@gmail.com>
2026-06-24 17:26:54 +0900

    ０６２４１７２６

---
+++ python/__pycache__/a47_try_except.py
@@ -0,0 +1,25 @@
+import math
+import sys
+
+
+def main():
+    user_input = input("정수 입력:")
+    try:
+        number_input = int(user_input)
+    
+    except ValueError as e:
+        print(e)
+        sys.exit()
+    
+    else:
+        print(f"원의 반지름: {number_input}")
+        print(f"원의 둘레: {number_input * 2 * math.pi}")
+        print(f"원의 넓이: {math.pi * number_input**2}")
+    
+    finally:
+        print("____ 프로그램 종료____")
+    
+    
+    
+if __name__ == "__main__":
+    main()
---
+++ python/a54_exception_custom.py
@@ -0,0 +1,28 @@
+import math
+
+class MinutsError(Exception):
+    def __init__ (self):
+        message = f"음수는 허용되지 않습니다"
+        super().__init__(message)
+        
+        
+def main():
+    user_input = input("양의 정수 입력:")
+    try:
+        number_input = int(user_input)
+        if number_input < 0:
+            raise MinutsError
+    
+    except ValueError as e:
+        print(e)
+    
+    else:
+        print(f"원의 반지름: {number_input}")
+        print(f"원의 둘레: {number_input * 2 * math.pi}")
+        print(f"원의 넓이: {math.pi * number_input**2}")
+    
+    finally:
+        print("____ 프로그램 종료____")
+
+if __name__ == "__main__":
+    main()
